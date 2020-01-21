8976cdb0dba835d178d0d7f270738fe44c150f74
spctr01 <rakshitkhpi@gmail.com>
2020-01-21 21:09:35 +0530

    update

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -1,27 +1,41 @@
-class Node:
-    def __init__(self,data):
+class node:
+    def __init__(self, data= None):
         self.data = data
-        self.next  = None
+        self.next = None
 
-        print(self.data)
-
-class LinkedList:
+class linked_list:
     def __init__(self):
-        self.headval = None
+        self.head = node()
 
-    def insert(self):
-        value = input('Node Value:')
-        if  self.headval == None:
-            self.headval = Node(value)
-        else:
-            print('walah')
+    def append(self,data):
+        new_node = node(data)
+        crnt =self.head
+        while crnt.next != None:
+            crnt = crnt.next
+        crnt.next = new_node
+
+    def length(self):
+        length = 0
+        start  = self.head
+        while start.next != None:
+            length += 1
+            start = start.next
+        return length
+
+    def display(self):
+        data = []
+        start = self.head
+        while start.next != None:
+            start = start.next
+            data.append(start.data)
+        return(data)
 
 
-l = LinkedList()
-while True:
-    usr_inp = input('1->input , anything else quit')
-    if usr_inp == '1':
-        l.insert()
-    else:break
-
+l = linked_list()
+print(l.length())
+l.append(3)
+l.append(2)
+l.append(4)
+print(l.length())
+print(l.display())
 
