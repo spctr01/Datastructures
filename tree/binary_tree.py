7318bb7ec4a9b08f51a4f0722525962172536db3
spctr01 <rakshitkhpi@gmail.com>
2020-01-28 15:37:52 +0530

    tree_insert

--- tree/binary_tree.py
+++ tree/binary_tree.py
@@ -1,9 +1,32 @@
-class node:
-    def __init__(self,data=None):
+class Node:
+    def __init__(self,data = None):
         self.data = data
-        self.right = None
         self.left = None
+        self.right = None
 
 class tree:
     def __init__(self):
-        pass
+        self.root = None
+
+    def insert(self, data):
+        if not self.root:
+            self.root = Node(data)
+        self.insert_node(data, self.root)
+
+    def insert_node(self, data, node):
+        if data > node.data:
+            if node.right:
+                self.insert_node(data,node.right)
+            node.right = Node(data)
+
+
+
+    def display (self):
+        b = self.root.right
+        print(self.root.data,b.data)
+
+t = tree()
+t.insert(5)
+t.insert(10)
+t.display()
+
