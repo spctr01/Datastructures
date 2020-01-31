3542d7bc020d75d423a36fa7b506d96c3c859cb3
spctr01 <rakshitkhpi@gmail.com>
2020-01-31 12:04:11 +0530

    preorder

--- tree/binary_tree.py
+++ tree/binary_tree.py
@@ -19,12 +19,14 @@ class tree:
         if data > node.data:
             if node.right:
                 self.insert_node(data,node.right)
-            node.right = Node(data)
+            else:
+                node.right = Node(data)
 
         else:
             if node.left:
                 self.insert_node(data, node.left)
-            node.left = Node(data)
+            else:
+                node.left = Node(data)
 
 
 #traversing tree INORDER
@@ -36,16 +38,36 @@ class tree:
         
         if node.right:
             self.traverse(node.right)
+            
+#travering PREORDER
+    def traverse_pre(self,node):
+        print(node.data)
+        if node.left:
+            self.traverse_pre(node.left)
         
+        if node.right:
+            self.traverse_pre(node.right)
 
 
     def display (self):
+        print('wallah inorder traverse.')
         if self.root:
             self.traverse(self.root)
+
+        print('wallah preorder traverse.')
+        if self.root:
+            self.traverse_pre(self.root)
+
+    
         
 
 t = tree()
-t.insert(5)
+t.insert(50)
+t.insert(20)
 t.insert(10)
+t.insert(30)
+t.insert(60)
+t.insert(55)
+t.insert(70)
 t.display()
 
