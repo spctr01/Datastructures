75076f7aa6c32aa4ade5f4da23c6c77dfca6a5b8
spctr01 <rakshitkhpi@gmail.com>
2020-02-07 15:08:13 +0530

    delete

--- tree/binary_tree.py
+++ tree/binary_tree.py
@@ -1,10 +1,13 @@
 class Node:
+    
     def __init__(self,data = None):
         self.data = data
         self.left = None
         self.right = None
 
+
 class tree:
+
     def __init__(self):
         self.root = None
 
@@ -14,49 +17,45 @@ class tree:
         else:
             self.insert_node(data, self.root)
         
-
     def insert_node(self, data, node):
         if data > node.data:
             if node.right:
                 self.insert_node(data,node.right)
             else:
                 node.right = Node(data)
-
         else:
             if node.left:
                 self.insert_node(data, node.left)
             else:
                 node.left = Node(data)
 
-
-    #traversing tree INORDER 
+    #traversing tree INORDER DEPTH FIRST SEARCH
     def trv(self,node):
         if node.left:
             self.trv(node.left)
 
         print(node.data, end=" ")
-        
+
         if node.right:
             self.trv(node.right)
             
-    #travering PREORDER 
+    #travering PREORDER DEPTH FIRST SEARCH
     def trv_pre(self,node):
         print(node.data, end=" ")
+
         if node.left:
             self.trv_pre(node.left)
-        
         if node.right:
             self.trv_pre(node.right)
 
-    #travering POSTORDER 
+    #travering POSTORDER DEPTH FIRST SEARCH
     def trv_post(self, node):
         if node.left:
             self.trv_post(node.left)
-
         if node.right:
             self.trv_post(node.right)
-        print(node.data , end =" ")
 
+        print(node.data , end =" ")
 
     #level order traversal BREADTH FIRST SEARCH
     def level(self,node):
@@ -65,35 +64,70 @@ class tree:
         
         while que:
             last_n = que[-1]
+
             if last_n.left:
                 que.insert(0,last_n.left)
-            
             if last_n.right:
-                que.insert(0,last_n.right)
-                
+                que.insert(0,last_n.right) 
+
             print(last_n.data,end=" ")
             del que[-1]
-        
+               
+    def remove(self, data, node):
+        if not node:
+            print('\ndata not found')
+            return None
+        if data > node.data:
+            node.right = self.remove(data, node.right)
+        elif data < node.data:
+            node.left = self.remove(data, node.left)
+        else:
+            if not node.left and not node.right:
+                del node
+                return None
+            if not node.left:
+                temp_mode = node.right
+                del node
+                return temp_node
+            elif not node.right:
+                temp_node = node.left
+                del node
+                return temp_node
 
+            #node with two chldren 
+            temp_node = self.left(node.left)
+            node.data = temp_node.data
+            node.left = self.remove(temp_node.data,node.left)
 
+        return node
+            
+    #return node with highest value from left subtree so that all values in left will be smaller than that
+    def left(self,node):
+        if node.right:
+            self.left(node.right)
+        return node
+
+    def delete(self,data):
+        if self.root:
+            self.remove(data, self.root)
+        else:
+            print('make a tree first ')
 
     def display (self):
-        print('\nwallah inorder traverse:', end =" ")
         if self.root:
+            print('\nwallah inorder traverse:', end =" ")
             self.trv(self.root)
- 
-        print('\nwallah preorder traverse:', end=" ")
-        if self.root:
+
+            print('\nwallah preorder traverse:', end=" ")
             self.trv_pre(self.root)
 
-        print('\nwallah postorder treversal:',end=" ")
-        if self.root:
+            print('\nwallah postorder treversal:',end=" ")
             self.trv_post(self.root)
 
-        print('\nwallah  level order traversal:', end=" ")
-        if self.root:
+            print('\nwallah  level order traversal:', end=" ")
             self.level(self.root)
-        
+
+
 
 t = tree()
 t.insert(50)
@@ -103,5 +137,7 @@ t.insert(30)
 t.insert(60)
 t.insert(55)
 t.insert(70)
+t.display()
+t.delete(0)
 t.display()
 
